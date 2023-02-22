d02dbb202cbc6fc3ef0b6f9ccc6ddb8f28610bf7
Vishruti Patil <vishruti2908@gmail.com>
2023-02-22 16:17:29 +0530

    ifelse_selection

---
+++ day 5/selection practice problem/ifelse/4.coinflip.sh
@@ -0,0 +1,15 @@
+# program to simulate a coin flip and print out "Heads" or "Tails" accordingly
+#!/bin/bash
+
+FLIP=$((RANDOM%2));
+
+if [ $FLIP -eq 1 ];
+then
+  
+echo "heads";
+
+else
+
+echo "tails";
+
+fi
